155fb33b19cff5546bc1638ed812162f1e60534d
Daniel C <daniel@daniel.com>
2019-12-14 11:44:32 -0600

    finished hellolist practice

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,15 +1,19 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+//@RequestMapping("/hello")
 public class HelloController {
 
 
     // Handles Requests at path  /hello (localhost:8080/hello/hellos)
+    @ResponseBody
     @GetMapping("hellos")
     public String hello() {
         return "Hello, Spring";
@@ -23,6 +27,7 @@ public class HelloController {
 
     // Handles Requests at path  /goodbye (localhost:8080/hello/goodbye)
     // now lives at (localhost:8080/hello/goodbye)
+    @ResponseBody
     @GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring";
@@ -30,63 +35,40 @@ public class HelloController {
 
     // Handles Requests at path  /dingo (localhost:8080/hello/dingo)
     // now @ hello/dingo
+    @ResponseBody
     @GetMapping("dingo")
     public String dingo() {
         return "Beware!! Dingos Live Here!!";
     }
 
-    // Handles Requests of the form /hello?name=LaunchCode (localhost:8080/hello/hello?name=LaunchCode)
-    // now lives @ hello/hello?name=
+    // Handles Requests of the form /hello?name=LaunchCode (localhost:8080/hello?name=LaunchCode)
+    // now lives @ hello?name=
     //@RequestMapping(method= {RequestMethod.GET, RequestMethod.POST})
     @RequestMapping(method= {RequestMethod.GET, RequestMethod.POST}, value= "hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name;
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String lochello = "Hello " + name + "!";
+        model.addAttribute("thhello", lochello);
+        return "hello";
     }
 
     // Handles Requests of the form /hello/stringName (localhost:8080/hello/stringName)
     @GetMapping("{name}")
     //@GetMapping("hello/{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name;
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String lochello = "Hello " + name + "!";
+        model.addAttribute("thhello", lochello);
+        return "hello";
     }
 
     // Handles Form Request
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action= 'create' method= 'post'>" +
-                "<input type= 'text' name= 'name'>" +
-                "<select name=\"language\">\n" +
-                "    <option value=\"\">--Please choose an option--</option>\n" +
-                "    <option value=\"en\">English</option>\n" +
-                "    <option value=\"sp\">Spanish</option>\n" +
-                "    <option value=\"fr\">French</option>\n" +
-                "    <option value=\"it\">Italian</option>\n" +
-                "    <option value=\"gr\">German</option>\n" +
-                "</select>\n" +
-                "<input type= 'submit' value= 'Enter name!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
-    /*
-    <form action="https://handlers.education.launchcode.org/request-parrot" method="POST">
-   <label>Username <input type="text" name="username"></label>
-   <label>Team Name <input type="text" name="team"></label>
-   <button>Submit</button>
-</form>
-
-     */
-
-//    @GetMapping("form")
-//    public String helloForm() {
-//        return createMessage("fr", "Zaphod");
-//    }
-//
     //@GetMapping("create")
-    @RequestMapping(method = RequestMethod.POST, value = "create")
+    @RequestMapping(method = RequestMethod.POST, value= "create")
+    //@RequestMapping(method = RequestMethod.POST)
     public static String createMessage(String language, String name) {
         if (language.equals("en")) {
             return "(" + language + ") " + "Hello " + name;
@@ -102,5 +84,21 @@ public class HelloController {
             return "(null) No Language Selected " + name;
         }
     }
+
+    @GetMapping("hellonames")
+    public String helloNames(Model model) {
+    List<String> locnames = new ArrayList<>();
+        locnames.add("Launch");
+        locnames.add("CodeLaunch");
+        locnames.add("LaunchCode");
+        locnames.add("LaunchRockets");
+        locnames.add("CodeRockets");
+        locnames.add("Rocket Code");
+        locnames.add("BlahBLAHBLAG");
+        model.addAttribute("thnames", locnames);
+        return "hellolist";
+    }
+
+
 
 }
